372db5a471e4c2db4460718470c9428f9e32fb4d
BernardoCubero <berniecubero@gmail.com>
2025-04-07 09:55:23 +0200

    Resuelto conflicto en CatalogoServicios.java

--- src/EXPractica/CatalogoServicios.java
+++ src/EXPractica/CatalogoServicios.java
@@ -19,13 +19,50 @@ public class CatalogoServicios implements Listable {
 
 	}
 
-	public void buscarServicio(ServicioTuristico servicio) {
+	public void buscarServicioDescripcion(String text) {
 		
+		//Reconoce que si la lista es vacia y nos informa
 		
+		if (servicios.isEmpty()) {
+			System.out.println("La lista esta vacia");
+
+		}
+		for (int i = 0; i < servicios.size(); i++) {
+			ServicioTuristico s = servicios.get(i);
+			if (s.getDescripcion().equalsIgnoreCase(text)) {
+				System.out.println(s);
+				return;
+
+			}
+		}
+		System.out.println("No se encuentra ningun codigo con esta descripción " + text);
+	}
+
+	public void buscarServicioCodigo(String codigo) {
+		// Nos indica si la lista esta vacia
+		if (servicios.isEmpty()) {
+			System.out.println("El catalogo esta vacio, No hay servivios que mostrar");
+			return;
+		}
+
+		for (ServicioTuristico p : servicios) {
+			if (p.getCodigo().equalsIgnoreCase(codigo)) {
+				System.out.println(p);
+				return;
+			}
+		}
+		System.out.println("No se encuentró ningun servivio con el codigo " + codigo);
 
 	}
 
+
 	public void eliminarServicio(ServicioTuristico servicio) {
+		
+		//Nos indica si la lista esta vacia 
+		
+		if (servicios.isEmpty()) {
+			System.out.println("La lista esta vacia");
+		}
 		System.out.println("Busca el codigo del servicio que quieres borrar");
 		String codigo = teclado.next();
 		for (ServicioTuristico p : servicios) {
@@ -36,16 +73,12 @@ public class CatalogoServicios implements Listable {
 
 	}
 
-	public void listadoCompleto(ServicioTuristico servicio) {
-		for (ServicioTuristico c : servicios) {
-			System.out.println(c);
-		}
 
-	}
 
 	@Override
 	public void mostrarCatalogoconPrecios() {
 		// TODO Auto-generated method stub
 
 	}
+
 }
